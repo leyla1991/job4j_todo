cac22f7aa94b861e4e9442c3029715de0f7e2de0
leyla1991 <atagleyla@mail.ru>
2024-10-12 16:47:59 +0300

    4. TODO list [#3786] Модель Task, Main.java

---
+++ src/main/java/ru/job4j/model/Task.java
@@ -0,0 +1,25 @@
+package ru.job4j.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tasks")
+@Data
+@RequiredArgsConstructor
+@NoArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NonNull
+    private String description;
+    @NonNull
+    private LocalDateTime created = LocalDateTime.now().withNano(0);
+    @NonNull
+    private boolean done;
+}
